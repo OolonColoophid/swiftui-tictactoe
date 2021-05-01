07f0fb2fd05c9eb8bda0c322f41c3ab48e506b06
Ian Hocking <ihocking@gmail.com>
2021-05-01 18:19:07 +0100

    Create basic interface.

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+
+    @State private var moves: [Move?] = Array(
+        repeating: nil,
+        count: 9)
+
+    @State private var isHumansTurn = true
+
+
+
     var body: some View {
-        Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+
+                LazyVGrid(
+                    columns: columns,
+                    spacing: 5
+                ) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.red).opacity(0.5)
+                                .frame(
+                                    width: geometry.size.width/3 - 15,
+                                    height:  geometry.size.width/3 - 15,
+                                    alignment: .center)
+
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(
+                                    width: 40,
+                                    height: 40,
+                                    alignment: .center)
+                                .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            moves[i] = Move(
+                                player: isHumansTurn ? .human : .computer,
+                                boardIndex: i)
+                            isHumansTurn.toggle()
+                        }
+                    }
+                }
+
+                Spacer()
+            }
             .padding()
+
+        }
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
---
+++ Tic-Tac-Toe/README.md
@@ -0,0 +1,9 @@
+#  Tic-Tac-Toe
+
+I'm  following the tutorial given by [this guy](https://www.youtube.com/watch?v=MCLiPW2ns2w), just to get a sense of what it might be like to write an app in SwiftUI.
+
+This will ultimately become an MVVM app (Model View ViewModel).
+
+## First Go
+
+Having fun! Got to 16:44.
